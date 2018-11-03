9b925bb3770afec18d9418868d8341a2757c5e22
Senpaii <dashaterdas@gmail.com>
2018-11-03 16:29:24 +0100

    -

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const bot = new Commando.Client({
     });
 const TOKEN = 'UR MOM IS FUCKING GAY'// no u
 
+
+let userData = JSON.parse(fs.readFileSync("data/userdata.json", "uft8"));
+
+
 bot.registry.registerGroup('simple', 'Simple')
 bot.registry.registerGroup('music', 'Music')
 bot.registry.registerDefaults();
@@ -12,30 +16,49 @@ bot.registry.registerCommandsIn(__dirname + "/commands")
 
 
 global.servers = {};//warum brauchst du das?        kannst du auch einfach auslassen im musikbot
-
 global.lastmessageuser = 0;
 global.lastmessagesend = 0;
+global.bannedwords = [      //das ist eine global variable weil ich noch später darauf zugreifen werden via eines commands
+        "nigger",
+
+];
 
 
 bot.on("message", function(message){
-
     const author =  message.author();// wehe du machst meinen code kapput @804 meins kode > deins kode xoxo
-
     if(bot.user == author)
     {return;}
+    var foundbadword = false;
 
-
+    for(var i in bannedwords)   //hier sucht der bot nach den blockierten wörtern
+    {
+        if(message.content.toLowerCase().includes(bannedwords[i].toLowerCase()))
+            foundbadword = true;
+    }
+    
+    if(foundbadword)//wenn er ein blockiertes wort gefunden hatt führt er das hier aus
+    {
+        message.delete();
+        author.send("`Please watch your language!`");
+    }
 
 });
 
 
-
-
 bot.on('ready',function(){
-    console.log("------------------------------------------------------------");
-    console.log(">>>>>>>>>>>>>>>>>>Started Bots successfully!<<<<<<<<<<<<<<<<");
-    console.log("------------------------------------------------------------");
+    console.log("######################################################################");
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>Started Bots successfully!<<<<<<<<<<<<<<<<<<<<<");
+    console.log("######################################################################");
     bot.user.setStatus('dnd');
+    let status = [
+        "tappin nns | selly.gg/@besteconfigs",
+        "http://hvh.academy/",
+        ""
+    ];
+    setInterval(function() {
+        let status2 = status[Math.floor(Math.random() * status.length)]
+        bot.user.setActivity(status2);
+    }, 10000)
     bot.user.setActivity('tappin nns | selly.gg/@besteconfigs', 'https://www.twitch.tv/twitch')
 });
 
