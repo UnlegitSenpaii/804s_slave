28369f7c6e214e40feadae349157e9e96b33f34b
Senpaii <dashaterdas@gmail.com>
2018-11-03 16:09:18 +0100

    uwU

--- index.js
+++ index.js
@@ -9,7 +9,27 @@ bot.registry.registerGroup('simple', 'Simple')
 bot.registry.registerGroup('music', 'Music')
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands")
-global.servers = {};
+
+
+global.servers = {};//warum brauchst du das?        kannst du auch einfach auslassen im musikbot
+
+global.lastmessageuser = 0;
+global.lastmessagesend = 0;
+
+
+bot.on("message", function(message){
+
+    const author =  message.author();// wehe du machst meinen code kapput @804 meins kode > deins kode xoxo
+
+    if(bot.user == author)
+    {return;}
+
+
+
+});
+
+
+
 
 bot.on('ready',function(){
     console.log("------------------------------------------------------------");
